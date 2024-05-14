481428ac2e76c610257d6cfdb2ebdce02a792743
Lukas <lukas.budginas@gmail.com>
2024-05-14 11:00:28 +0300

    Update root ui files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/register";
+import Login from "./pages/login";
+import Dashoard from "./pages/dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Register />}
+        />
+        <Route
+          path="login"
+          element={<Login />}
+        />
+        {/* Show content when user is authenticated*/}
+        <Route
+          path="dashboard"
+          element={<Dashoard />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
